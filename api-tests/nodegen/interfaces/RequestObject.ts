216d8424c74b9a0d8733940faf4bd6aadcf55c5a
John Carmichael <carmichael@acrontum.de>
2020-01-14 12:21:21 +0100

    feat: accommodate formData

--- api-tests/nodegen/interfaces/RequestObject.ts
+++ api-tests/nodegen/interfaces/RequestObject.ts
@@ -3,6 +3,7 @@ import {HttpMethodEnum} from '../enums/HttpMethodEnum'
 export interface RequestObject {
   method: HttpMethodEnum
   path: string
+  formData?: object
   params?: object
   qs?: object
   body?: object
